8070f65ad8416d75335d258309dab6b7d235f9f1
HARIBO8 <opktruth8@gmail.com>
2025-06-19 00:49:38 +0800

    あ

--- twitter/urls.py
+++ twitter/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('home/', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
